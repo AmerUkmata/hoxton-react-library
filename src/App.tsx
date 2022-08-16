af52764b2740b219ef5e5ef9cf811686d5995495
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-16 22:45:17 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 // - Start a new project✅
 // - Create components, similar to the Button component Nico built during the lecture. ✅
-//   - Button: should return a button styled however you like. Make it use a few useful custom props similar to how Nico used them during the lecture - like size or variant.
-//     - Input: returns a label and an input under it, both customizable.
+//   - Button: should return a button styled however you like. Make it use a few useful custom props similar to how Nico used them during the lecture - like size or variant.✅
+//     - Input: returns a label and an input under it, both customizable.✅
 //   - CheckboxGroup: a group of checkboxes. The component should take:
 //     - an array of items and render a list of checkboxes based on them. (Think what should go into the array!)
 //     - an onSelect: selecting a checkbox should run the function. Make sure you can check which checkbox you selected.
